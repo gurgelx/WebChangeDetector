ee9def1feef82dbdaa5775e7cd37861fdd963460
Gustaf Brahme <gustaf@brahme.se>
2021-08-15 11:04:44 -0500

    working poc

---
+++ WebChangeDetector/Checker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace WebChangeDetector
+{
+    public class Checker
+    {
+        private string lastValue = null;
+        public string TargetUrl { get; private set; }
+        public Action<string> OnDetectionCallback { get; private set; }
+        public Func<RemoteWebDriver, string> ValueFinder {get; private set;}
+
+        public Checker(string url, Func<RemoteWebDriver, string> valueFinder, Action<string> onDetectionCallback)
+        {
+            this.TargetUrl = url;
+            this.OnDetectionCallback = onDetectionCallback;
+            this.ValueFinder = valueFinder;
+        }
+
+        public void RunWithIntervall(int intervall)
+        {
+            while (true)
+            {
+                DoCheck();
+                System.Threading.Thread.Sleep(intervall);
+            }
+        }
+
+        void DoCheck()
+        {
+            var options = new ChromeOptions();
+            options.AddArguments("headless");
+            using (var driver = new ChromeDriver(options))
+            {
+                driver.Navigate().GoToUrl(TargetUrl);
+                var value = this.ValueFinder(driver);
+                if (lastValue != null && !lastValue.Equals(value))
+                {
+                    this.OnDetectionCallback(value);
+                }
+                lastValue = value;                
+            }
+        }
+    }
+}
---
+++ WebChangeDetector/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Diagnostics;
+
+namespace WebChangeDetector
+{
+    class Program
+    {
+        static string TargetUrl = "https://swedroid.se/forum/threads/fyndtipstraden-amazon-se-inga-diskussioner.186347/";
+        static int CheckIntervall = 1000 * 10; //1000ms = 1s x 60 = 1min
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running checks on " + TargetUrl);
+            var checker = new Checker(TargetUrl, ElementFinder, OnChangeDetected);
+            checker.RunWithIntervall(CheckIntervall);
+        }
+
+        static string ElementFinder(RemoteWebDriver driver)
+        {
+            return driver
+                .FindElement(By.ClassName("pageNavLinkGroup"))
+                .FindElement(By.TagName("nav"))
+                .FindElements(By.XPath("./a"))[3].Text;
+
+        }
+
+        static void OnChangeDetected(string newValue)
+        {
+            var lastPage = "https://swedroid.se/forum/threads/fyndtipstraden-amazon-se-inga-diskussioner.186347/page-" + newValue;
+            Process.Start(new ProcessStartInfo(lastPage) { UseShellExecute = true });
+        }
+    }
+}
