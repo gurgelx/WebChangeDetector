2b3f6c0eecdf7041016830401bce932224e83276
Gustaf B <gustaf@brahme.se>
2021-09-28 22:50:54 +0200

    added stuff for discord webhooks

---
+++ DiscordAPI/DiscordObjects.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace DiscordAPI
+{
+    class DiscordContent
+    {
+        public string Content { get; set; }
+        public List<DiscordComponent> Components { get; set; }
+        public List<DiscordEmbedObject> Embeds { get; set; }
+
+    }
+
+    class DiscordComponent
+    {
+        public int Type { get; set; }
+        public List<DiscordComponent> Components { get; set; }
+
+        public List<string> Embeds { get; set; }
+
+    }
+    class DiscordEmbedObject
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+    }
+
+    class DiscordButtonComponent : DiscordComponent
+    {
+        public string Label { get; set; }
+        public int Style { get; set; }
+        public string CustomId { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ DiscordAPI/DiscordWebhook.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordAPI
+{
+    public class DiscordWebhook
+    {
+        private int buttonId = 0;
+        private readonly string webhookUrl;
+
+        public DiscordWebhook(string webhookUrl)
+        {
+            this.webhookUrl = webhookUrl;
+        }
+
+        public async Task<bool> Push(string text, string url)
+        {
+            var client = new HttpClient();
+
+
+            DefaultContractResolver contractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new SnakeCaseNamingStrategy()
+            };
+
+            var serializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = contractResolver,
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            var json = JsonConvert.SerializeObject(CreatePackage("Ny post", text, url), serializerSettings);
+            
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var result = await client.PostAsync(webhookUrl, content);
+            client.Dispose();
+            return result.IsSuccessStatusCode;
+        }
+
+        private DiscordContent CreatePackage(string title, string text, string url)
+        {
+            return new DiscordContent
+            {               
+               Embeds = new List<DiscordEmbedObject>
+               {
+                   new DiscordEmbedObject
+                   {
+                       Title = title,
+                       Url = url,
+                       Description = text
+                   }
+               }                
+            };
+        }
+
+        private DiscordComponent CreateButton(string text, string url)
+        {
+            return new DiscordButtonComponent()
+            {
+                Type = 2,
+                Url = url,
+                CustomId = "button-id-" + buttonId++,
+                Label = text,
+                Style = 5
+            };
+        }
+    }   
+
+}
